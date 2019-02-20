40419d7d092e07d6a0f4783488a9263afd6559e0
Yigit Demirag <yigitdemirag@protonmail.ch>
2019-02-20 03:56:22 +0300

    working vpg

--- RL/agents/model.py
+++ RL/agents/model.py
@@ -1,35 +1,79 @@
 import numpy as np
+import scipy.signal
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.distributions.categorical import Categorical
+from torch.distributions.normal import Normal
+from gym.spaces import Box, Discrete
+
+from RL.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
 
 class ReplayBuffer:
     def __init__(self, size, obs_dim, act_dim):
         self.size = size
         self.o_buff = np.zeros((size, *obs_dim), dtype=np.float32)
+        self.logp_buff = torch.zeros((size, *act_dim), dtype=torch.float32) 
         self.a_buff = np.zeros((size, *act_dim), dtype=np.float32)
         self.rew_buff = np.zeros((size), dtype=np.float32)
+        self.rew2g_buff = np.zeros((size), dtype=np.float32)
         self.val_buff = np.zeros((size), dtype=np.float32)
+        self.adv_buff = np.zeros((size), dtype=np.float32)
+        self.start_ptr = 0
+        self.gamma = 0.99
+        self.lam = 0.95
         self.ptr = 0
 
     def read(self):
         """
-        Read from the replay buffer and reset its content.
+        Read from the replay buffer in any random time..
         """
-        self.ptr = 0
-        return [self.o_buff, self.a_buff, self.rew_buff, self.val_buff] 
-
-    def write(self, obs, act, rew, val):
+        self.calc_advantage()
+        tmp = [self.o_buff, self.logp_buff, self.a_buff, self.rew_buff, self.rew2g_buff, self.val_buff, self.adv_buff]
+        return [torch.Tensor(x) for x in tmp]
+   
+    def write(self, obs, logp, act, rew, val):
         """
-        Write to cyclical replay buffer after every interaction.
+        Write to replay buffer after every interaction with env.
         """
-        if self.size == self.ptr:
+        if self.ptr == self.size:
+            self.start_ptr = 0
             self.ptr = 0 
         
         self.o_buff[self.ptr] = obs
+        self.logp_buff[self.ptr] = logp
         self.a_buff[self.ptr] = act
         self.rew_buff[self.ptr] = rew # The reward agent currently helds, not the one it will get after act in obs.
         self.val_buff[self.ptr] = val
         self.ptr += 1
+
+    def discount_cumsum(self, x, discount):
+        """
+        Return discount on given vector : x0 + gamma*x1 + gamma^2*x2 + gamma^3*x3 ...
+        """
+        return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+    
+    def calc_advantage(self, last_val=0):
+        """
+        Calculate GAE-lambda advantage function 
+        """
+        path_slice = slice(self.start_ptr, self.ptr)
+        rews = np.append(self.rew_buff[path_slice], last_val)
+        vals = np.append(self.val_buff[path_slice], last_val)
+        
+        # Calculate GAE-lambda
+        deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
+        self.adv_buff[path_slice] = self.discount_cumsum(deltas, self.gamma * self.lam)
+
+        # Calculate rewards to go
+        self.rew2g_buff[path_slice] = self.discount_cumsum(rews, self.gamma)[:-1]
+
+        # Normalize A with mu=0, std=1
+        adv_mean, adv_std = mpi_statistics_scalar(self.adv_buff)
+        self.adv_buff = (self.adv_buff-adv_mean)/adv_std
+
+        self.start_ptr = self.ptr
 
 class MLP(nn.Module):
     def __init__(self, x, hidden_sizes=(32,32), activation=torch.tanh, output_activation=None):
@@ -37,26 +81,68 @@ class MLP(nn.Module):
         self.activation = activation
         self.output_activation = output_activation
         self.layers = nn.ModuleList()  
-        
+        print('Hidden sizes:')
         for i, h in enumerate([x[0]] + list(hidden_sizes[:-1])): # x has shape of (a, ) and hidden_sizes is (a,b,c,..)
-            #print((h, hidden_sizes[i]))
+            print((h, hidden_sizes[i]))
             self.layers.append(nn.Linear(h, hidden_sizes[i]))
        
     def forward(self, x):
         for layer in self.layers[:-1]:
             x = self.activation(layer(x))
-        x = self.output_activation(self.layers[-1](x)) if self.output_activation!=None else self.layers[-1](x)
+        x = self.output_activation(self.layers[-1](x)) if self.output_activation != None else self.layers[-1](x)
         return x
 
-
-
 '''
-class ActorCritic(nn.Module,):
-    def __init__(self):
-
-
-    self.policy = 
-
-
-    self.value = 
+Policies
 '''
+class mlp_categorical_policy(nn.Module):
+    def __init__(self, x, a, hidden_sizes, activation, output_activation, action_space):
+        super(mlp_categorical_policy, self).__init__()
+        self.act_dim = action_space.n
+        self.p_net = MLP(x, list(hidden_sizes)+[self.act_dim], activation, None)
+        
+    def forward(self, x, a=None):
+        logits = self.p_net(x)
+        policy = Categorical(logits=logits) # Provides better functions, use instead of torch.multinomial()
+        pi = policy.sample() # Action agent takes 
+        logp = policy.log_prob(a) if torch.is_tensor(a) else None # log probability of taking action a 
+        logp_pi = policy.log_prob(pi) # log probability of taking action a by policy pi
+        return pi, logp, logp_pi
+
+class mlp_gaussian_policy(nn.Module):
+    def __init__(self, x, a, hidden_sizes, activation, output_activation, action_space):
+        super(mlp_gaussian_policy, self).__init__()
+        self.act_dim = action_space.shape[0]
+        self.p_net = MLP(x, list(hidden_sizes)+[self.act_dim], activation, output_activation)
+        self.log_std = nn.Parameter(-0.5*torch.ones(self.act_dim,dtype=torch.float32))
+
+    def forward(self, x, a=None):
+        mu = self.p_net(x)
+        policy = Normal(mu, self.log_std.exp())
+
+        pi = policy.sample()
+        logp = policy.log_prob(a) if torch.is_tensor(a) else None
+        logp_pi = policy.log_prob(pi)
+        return pi, logp, logp_pi
+
+
+class mlp_actor_critic(nn.Module):
+    def __init__(self, x, a, hidden_sizes=(64,64), activation=torch.tanh, 
+        output_activation=None, policy=None, action_space=None):
+        super(mlp_actor_critic, self).__init__()
+        
+        # Policy Network 
+        if policy is None and isinstance(action_space, Box):
+            print('Policy is None and action_space is Box.')
+            self.p_net = mlp_gaussian_policy(x, a, hidden_sizes, activation, output_activation, action_space)
+        elif policy is None and isinstance(action_space, Discrete):
+            print('Policy is None and action_space is Discrete.')
+            self.p_net = mlp_categorical_policy(x, a, hidden_sizes, activation, output_activation, action_space)
+
+        # Value Network
+        self.v_net = MLP(x, list(hidden_sizes)+[1], activation, None) 
+
+    def forward(self, x, a=None):
+        pi, logp, logp_pi = self.p_net(x, a)
+        v = self.v_net(x)
+        return pi, logp, logp_pi, v
--- RL/agents/vpg/vpg.py
+++ RL/agents/vpg/vpg.py
@@ -4,54 +4,80 @@ import time
 import numpy as np
 import torch 
 import torch.nn as nn
+import RL.agents.model as model
 
 from RL.utils.logx import EpochLogger
 from RL.utils.mpi_torch import average_gradients, sync_all_params
 from RL.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
-from RL.agents.model import ReplayBuffer, MLP
+from RL.agents.model import ReplayBuffer, mlp_actor_critic
 
 # Hyperparameters
 epochs = 100
 steps_per_epoch = 1000
 
-def vpg(env_fn, seed=0, logger_kwargs=dict()):
+def vpg(env_fn, actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=dict()):
     """
     Vanilla Policy Gradient Algorithm implemented with GAE-lambda advantage function.
     """
     # Loggers
     logger = EpochLogger(**logger_kwargs)
     logger.save_config(locals())
+    
+    # Update the seed
+    seed = 1000 * proc_id()
+    torch.manual_seed(seed)
+
+    env = env_fn()
+    obs_dim = env.observation_space.shape
+    act_dim = env.action_space.shape
 
     local_steps_per_epoch = int(steps_per_epoch/num_procs())
-    env = env_fn()
+    
+    actor_critic = mlp_actor_critic(obs_dim, act_dim, action_space=env.action_space)
     rb = ReplayBuffer(local_steps_per_epoch, env.observation_space.shape, env.action_space.shape)
     
-    done = False
-    ep_ret = ep_len = r = val = 0
+    # Optimizer
+    p_optimizer = torch.optim.Adam(actor_critic.p_net.parameters(), lr = 3e-4)
+    v_optimizer = torch.optim.Adam(actor_critic.v_net.parameters(), lr = 1e-3)
+    sync_all_params(actor_critic.parameters())
+
+    def update():
+        o, logp, a, _, rew2g, val, adv = rb.read()
+        _, logp, _, val = actor_critic(o,a) 
+       
+        p_loss = -(logp * adv).mean()
+        p_optimizer.zero_grad()
+        p_loss.backward()
+        p_optimizer.step()
+
+        v_loss = (val - rew2g).pow(2).mean()
+        v_optimizer.zero_grad()
+        v_loss.backward()
+        v_optimizer.step()
+
+    # Initializations
+    ep_ret, ep_len, r, val, done = 0, 0, 0, 0, False
     start_time = time.time()
 
-####
-# DELETE LATER
-    mlp = MLP(env.observation_space.shape, (32,64,4), activation=torch.tanh) 
-###
     # Agent in the Wild 
     for epoch in range(epochs):
-        s = env.reset()
+        obs = env.reset()
         for t in range(local_steps_per_epoch):
-            env.render()
-            a = env.action_space.sample()
-            rb.write(s,a,r,val)
+            #env.render()
+            a, _, logp, v = actor_critic(torch.FloatTensor(obs))
+            rb.write(obs, logp, a, r, v)
+            obs, r, done, _ = env.step(a.detach().numpy())
             
-            s, r, done, _ = env.step(a)
-
             ep_ret += r
             ep_len += 1
             
             # Terminal state
             if done or t==local_steps_per_epoch:
                 logger.store(EpRet=ep_ret, EpLen=ep_len)
-                s, r, done, ep_ret, ep_len = env.reset(), 0,  False, 0, 0
+                obs, r, done, ep_ret, ep_len = env.reset(), 0,  False, 0, 0
 
+
+        update()
         # Logger Monitor
         logger.log_tabular('Epoch', epoch)
         logger.log_tabular('EpRet', with_min_and_max=True)
@@ -67,8 +93,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='LunarLander-v2')
     parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--hid', type=int, default=64)
     args = parser.parse_args()
     logger_kwargs = setup_logger_kwargs('vpg', 0)
     mpi_fork(args.cpu)
     
-    vpg(lambda: gym.make(args.env), seed=0, logger_kwargs=logger_kwargs)
+    vpg(lambda: gym.make(args.env), actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=logger_kwargs)
