435f73ec46ade271cf37262d7f183023337f6145
Yigit Demirag <yigitdemirag@protonmail.ch>
2019-03-01 03:43:57 +0300

    vpg is updated

--- RL/agents/vpg/vpg.py
+++ RL/agents/vpg/vpg.py
@@ -11,7 +11,7 @@ from RL.utils.mpi_torch import average_gradients, sync_all_params
 from RL.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
 from RL.agents.model import ReplayBuffer, mlp_actor_critic
 
-def vpg(env_fn, actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=dict()):
+def vpg(env_fn, actor_critic=model.mlp_actor_critic, ac_kwargs=dict(), seed=0, steps_per_epoch=4000, epochs=50, p_lr=3e-4, v_lr=1e-3, logger_kwargs=dict(), save_freq=10):
     """
     Vanilla Policy Gradient Algorithm implemented with GAE-lambda advantage function.
     """
@@ -20,19 +20,18 @@ def vpg(env_fn, actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=dict(
     logger.save_config(locals())
     
     # Update the seed
-    torch.manual_seed(1000*proc_id())
-   
+    seed += 1000*proc_id()
+    torch.manual_seed(seed)
 
-    epochs, steps_per_epoch = 300, 1000
     local_steps_per_epoch = int(steps_per_epoch/num_procs())
-    
     env = env_fn()
-    actor_critic = mlp_actor_critic(env.observation_space.shape, env.action_space.shape, action_space=env.action_space)
+
+    actor_critic = mlp_actor_critic(env.observation_space.shape, action_space=env.action_space, **ac_kwargs)
     rb = ReplayBuffer(local_steps_per_epoch, env.observation_space.shape, env.action_space.shape)
     
     # Optimizers
-    p_optimizer = torch.optim.Adam(actor_critic.p_net.parameters(), lr = 3e-4)
-    v_optimizer = torch.optim.Adam(actor_critic.v_net.parameters(), lr = 1e-3)
+    p_optimizer = torch.optim.Adam(actor_critic.p_net.parameters(), lr = p_lr)
+    v_optimizer = torch.optim.Adam(actor_critic.v_net.parameters(), lr = v_lr)
     sync_all_params(actor_critic.parameters())
 
     # Initializations
@@ -40,7 +39,7 @@ def vpg(env_fn, actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=dict(
     start_time = time.time()
     
     def update():
-        o, logp, a, _, rew2g, val, adv = rb.read()
+        o, logp, a, _, rew2g, val, adv = rb.read(on_policy=True)
         _, logp, _, val = actor_critic(o,a) 
       
         p_loss = -(logp * adv).mean()
@@ -66,13 +65,16 @@ def vpg(env_fn, actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=dict(
             ep_ret += r
             ep_len += 1
             
-            # Terminal state
-            if done or t==local_steps_per_epoch:
-                logger.store(EpRet=ep_ret, EpLen=ep_len)
+            # Do not lose r,v at terminal states
+            if done or t==local_steps_per_epoch-1:
+                v_d = r if done else actor_critic.v_net(torch.Tensor(obs)).item()
+                rb.calc_adv(v_d)
+                if done:
+                    logger.store(EpRet=ep_ret, EpLen=ep_len)
                 obs, r, done, ep_ret, ep_len = env.reset(), 0,  False, 0, 0
         
         # Save model
-        if (epoch % 20 == 0) or (epoch == epochs-1):
+        if (epoch % save_freq == 0) or (epoch == epochs-1):
             logger.save_state({'env': env}, actor_critic, None)
 
         update()
@@ -90,11 +92,23 @@ if __name__ == '__main__':
     from RL.utils.run_utils import setup_logger_kwargs
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='LunarLander-v2')
-    parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--hid', type=int, default=64)
+    parser.add_argument('--cpu', type=int, default=8)
+    parser.add_argument('--hid', type=int, default=32)
+    parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--exp_name', type=str, default='vpg')
     args = parser.parse_args()
-    logger_kwargs = setup_logger_kwargs('vpg', 0)
+    logger_kwargs = setup_logger_kwargs(args.exp_name, 0)
+    
     mpi_fork(args.cpu)
     
-    vpg(lambda: gym.make(args.env), actor_critic=model.mlp_actor_critic, seed=0, logger_kwargs=logger_kwargs)
+    vpg(lambda: gym.make(args.env), 
+        actor_critic=model.mlp_actor_critic, 
+        ac_kwargs=dict(hidden_sizes=[args.hid] * args.l), 
+        seed=0, 
+        steps_per_epoch=4000, 
+        epochs=50, 
+        p_lr=3e-4, 
+        v_lr=1e-3, 
+        logger_kwargs=dict(), 
+        save_freq=10)
